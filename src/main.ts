54ab1915c3923d9ad962293d2efc7358ec90f6ab
Raul Valdovinos <tu-correo@email.com>
2026-04-19 21:02:25 -0400

    feat: setup frontend react y formulario inicial de proyecto

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -14,7 +17,7 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-  
+
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
 
